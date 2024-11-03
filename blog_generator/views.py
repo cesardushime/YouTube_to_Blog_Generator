bcf4f743859fab7d57da33173a80d6907f2f55ab
cesardushime <125463453+cesardushime@users.noreply.github.com>
2024-11-03 10:56:42 +0100

    added user authentication: signup, login, and logout

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,14 +1,52 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
+from pytube import YouTube
 # Creating veiws and rendering templates.
 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
+def generate_blog(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            yt_link = data['link']
+        except(KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invalid request method'}, status=405) 
+        
+        # get yt title
+        title = yt_title(yt_link)
+        # get transcript
+        # user OpenAI to generate the blog
+        # Save blog article to database
+        # return blog article as a response
+    else:
+        return JsonResponse({'error': 'Invalid data sent'}, status=400)
+def yt_title(link):
+    yt = YouTube(link)
+    title = yt.title
+    return title
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']  
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = "Invalid password!"
+            return render(request, 'login.html', {'error_message': error_message})
     return render(request, 'login.html')
+
 def user_signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -30,4 +68,5 @@ def user_signup(request):
             return render(request, 'signup.html', {'error_message': error_message})
     return render(request, 'signup.html')
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')
