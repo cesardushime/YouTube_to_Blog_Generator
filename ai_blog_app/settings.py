398ca9ea7d4df7c3e3f63ca157662ea567e7661d
cesardushime <125463453+cesardushime@users.noreply.github.com>
2024-11-24 10:37:56 +0100

    more tests

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_URL = 'login'
+LOGIN_URL = '/login'
--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('blog_list', views.blog_list, name='blog_list'),
     path('blog-details/<int:pk>/', views.blog_details, name='blog-details'),
 
-
 ]
